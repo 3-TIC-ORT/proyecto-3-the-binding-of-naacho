990d4f30a49cff68d9c966cc852f243ee3b1607c
Coolfede97 <coolfede97@gmail.com>
2024-08-03 23:41:15 -0300

    Avance con la generación de mazmorras aleatorias
    Falta solucionar un problema en el cual pueden quedar salas con puertas abiertas que dan al vacío interminable y depresivo :v

---
+++ Assets/Dungeon/Dungeon Sprites/RoomSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    private GameObject grid;
+    public int openingDirection;
+    // 1 Down door     2 Top door        3  Left door       4 Right door
+
+    private RoomTemplates templates;
+    private bool spawned = false;
+    void Start()
+    {
+        grid = GameObject.Find("Grid");
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (!spawned)
+        {
+            // Verificar si el espacio está vacío antes de instanciar
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.0001f);
+            if (colliders.Length <=1)
+            {
+                Debug.Log("ASD");
+
+                if (openingDirection == 1)
+                {
+                    int rand = Random.Range(0, templates.downRooms.Length);
+                    Instantiate(templates.downRooms[rand], transform.position, Quaternion.identity, grid.transform);
+                }
+                else if (openingDirection == 2)
+                {
+                    int rand = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity, grid.transform);
+                }
+                else if (openingDirection == 3)
+                {
+                    int rand = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity, grid.transform);
+                }
+                else if (openingDirection == 4)
+                {
+                    int rand = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity, grid.transform);
+                }
+                spawned = true;
+            }
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("SpawnPoint") && col.GetComponent<RoomSpawner>().spawned)
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
---
+++ Assets/Dungeon/Dungeon Sprites/RoomTemplates.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] downRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
