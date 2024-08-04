e10941b57b91b2021a19dcf419bab1214ccce6d9
Tomás Atanasoff <tomyatana@gmail.com>
2024-08-03 21:20:30 -0300

    Fixed movement key detection

--- Assets/Scripts/NaachoController.cs
+++ Assets/Scripts/NaachoController.cs
@@ -13,14 +13,30 @@ public class NaachoController : MonoBehaviour
         rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    Vector2 getMovement() 
+    {
+        int horizontalMovement = 0;
+        int verticalMovement = 0;
+
+        if(Input.GetKey(KeyCode.D))
+            horizontalMovement = 1;
+        else if(Input.GetKey(KeyCode.A))
+            horizontalMovement = -1;
+        
+        if(Input.GetKey(KeyCode.W))
+            verticalMovement = 1;
+        else if(Input.GetKey(KeyCode.S))
+            verticalMovement = -1;
+
+        return new Vector2(horizontalMovement, verticalMovement);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float verticalMovement = Input.GetAxis("Vertical");
-        float horizontalMovement = Input.GetAxis("Horizontal");
-    
-        if(verticalMovement != 0 || horizontalMovement != 0)
-            rb2D.velocity = new Vector2(horizontalMovement, verticalMovement).normalized * Speed * Time.deltaTime;
+        Vector2 movement = getMovement().normalized;
+        if(movement.x != 0 || movement.y != 0)
+            rb2D.velocity = movement * Speed * Time.deltaTime;
         else
             rb2D.velocity *= Friction;
     }
