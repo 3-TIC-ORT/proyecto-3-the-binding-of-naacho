c80163b45f558e9458bbaf95398ab87e9d56793f
Tomás Atanasoff <tomyatana@gmail.com>
2024-08-04 13:54:23 -0300

    Finished proj script

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,13 +7,20 @@ public class Projectile : MonoBehaviour
     public string proj_name;
     public string proj_tag;
 
-   private GameObject proj;
+    private GameObject proj;
 
-
-    public GameObject createProjectile(string name, Vector2 position, bool enemy, Vector2 direction, Vector2 velocity)
+    public GameObject createProjectile(string name, Vector2 position, Vector2 scale, bool enemy, Vector2 direction, int speed, Vector2 extraVelocity)
     {
         GameObject proj = new GameObject(name);
         Rigidbody2D proj_rb2D = proj.AddComponent<Rigidbody2D>();
+        
+        proj.transform.localScale = scale;
+
+        proj.AddComponent<BoxCollider2D>();
+
+        proj_rb2D.velocity = direction * speed + extraVelocity;
+        proj.transform.position = position;
+
         return proj;
     }
 }
