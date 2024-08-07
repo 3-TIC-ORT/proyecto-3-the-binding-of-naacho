7e94f63b2af2f7893e4334c934b6c3900bfbb61c
Tomás Atanasoff <tomyatana@gmail.com>
2024-08-06 21:05:02 -0300

    Created enemy fields

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    protected float HealthPoints;
+    protected float DamagePoints;
+
+    protected uint Speed;
+
+    protected ProjectileCreator projectileCreator;
+    protected Sprite EnemySprite;
+    protected Collider2D Col2D;
+    protected Rigidbody2D rb2D;
+
+
     // Start is called before the first frame update
     void Start()
     {
