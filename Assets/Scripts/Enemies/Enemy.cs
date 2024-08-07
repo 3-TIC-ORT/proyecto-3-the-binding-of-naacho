a873d60e2aad070102c49443a1c613dd7b7bd8eb
Tomás Atanasoff <tomyatana@gmail.com>
2024-08-06 21:10:22 -0300

    Started creating enemy constructor

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    protected string Name;
+
+    protected GameObject EnemyObj;
+
     protected float HealthPoints;
     protected float DamagePoints;
 
@@ -14,6 +19,9 @@ public class Enemy : MonoBehaviour
     protected Collider2D Col2D;
     protected Rigidbody2D rb2D;
 
+    Enemy(Collider2D Col2D, Sprite sprite, float hp = 3f, float dp = 0.5f, uint speed = 350, string name = "Enemy") {
+        
+    }
 
     // Start is called before the first frame update
     void Start()
