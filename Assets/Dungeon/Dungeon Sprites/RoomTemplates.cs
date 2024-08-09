8a78cfd28253d12e3f715f70f264636b2dd2e6a9
Coolfede97 <coolfede97@gmail.com>
2024-08-09 10:30:37 -0300

    avance

--- Assets/Dungeon/Dungeon Sprites/RoomTemplates.cs
+++ Assets/Dungeon/Dungeon Sprites/RoomTemplates.cs
@@ -6,13 +6,19 @@ public class RoomTemplates : MonoBehaviour
 {
     public int roomsGenerated = 0;
     public int roomsLimit; // No es preciso
+    [Header("Room List")]
     public GameObject[] downRooms;
     public GameObject[] topRooms;
     public GameObject[] leftRooms;
     public GameObject[] rightRooms;
-
     public GameObject closedRoom;
-
+    [Header("Not Closing Room List")]
+    public GameObject[] NTdownRooms;
+    public GameObject[] NTtopRooms;
+    public GameObject[] NTleftRooms;
+    public GameObject[] NTrightRooms;
+    public GameObject NTclosedRoom;
+    [Header("Other")]
     public GameObject roomConector;
     void Start()
     {
