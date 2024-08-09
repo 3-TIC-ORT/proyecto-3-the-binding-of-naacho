02c6a4c97a420c3cc6fcb95e6933402e964067a9
Tomyatana <tomyatana@gmail.com>
2024-08-09 14:15:50 -0300

    Trying to add life

--- Assets/Scripts/Naacho/NaachoHeartSystem.cs
+++ Assets/Scripts/Naacho/NaachoHeartSystem.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+
+enum HeartTypes
+{
+   Normal,
+}
+
+class Heart {
+    public HeartTypes heartType = HeartTypes.Normal;
+    public float Amount;
+
+    public Heart(float amount = 1f, HeartTypes ht = HeartTypes.Normal) {
+        heartType = ht;
+        Amount = amount;
+    }
+
+    protected void Damage(float dp = .5f) {
+        Amount -= dp;
+    }
+
+    protected void Heal(float hp = .5f) {
+        Amount = (Amount + hp <= 1f) ? Amount + hp : Amount;
+    }
+}
 
 public class NaachoHeartSystem : MonoBehaviour
 {
-    public enum HeartTypes
-    {
-        Normal,
-    }
-    public HeartTypes[] Hearts;
+    public int startingLife = 3;
+    List<Heart> Life;
+    public float LifeAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Life = new List<Heart>(startingLife);
+        for(int i = 0; Life.Capacity > i; i++) {
+            Life[i] = new Heart();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        LifeAmount = 0;
+        for(int i = 0; Life.Capacity > i; i++) {
+            LifeAmount += Life[i].Amount;
+        }
     }
 }
