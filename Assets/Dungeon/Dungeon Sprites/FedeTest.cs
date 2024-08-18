a9d19392a9d917c194e488e08f820f4c527f11c5
Coolfede97 <coolfede97@gmail.com>
2024-08-18 00:51:26 -0300

    LISTOOOO, valió la pena tanto tiempo :'v
    Desde las 6 de la tarde hasta las  0:49 de la noche, fede consiguió el 18/8/2024 resolver este problema. Ahora hay habitaciones conectadas grandes.
    Se resolvió el problema aunque no estoy para nada seguro porque ocurría el problema. Se que tiene que ver con que la tag del col del ontriggerenter2d y que no era room, pero debería.
    
    Cosas de la vida.

--- Assets/Dungeon/Dungeon Sprites/FedeTest.cs
+++ Assets/Dungeon/Dungeon Sprites/FedeTest.cs
@@ -17,45 +17,9 @@ public class FedeTest : MonoBehaviour
     {
         
     }
-    private void OnTriggerStay2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!destroyedWalls && !col.CompareTag("SpawnPoint"))
-        {
+        //Debug.Log("holdapsalsd");
 
-            destroyedWalls = true;
-            Vector2[] lugares0 = { Vector2.right, Vector2.up, Vector2.left,Vector2.left, Vector2.down, Vector2.down, Vector2.right, Vector2.right };
-            List<Vector2> lugares = new List<Vector2>(lugares0);
-            foreach (Vector2 v in lugares)
-            {
-                Vector3 collisionPoint = col.ClosestPoint(transform.position);
-                Debug.Log(collisionPoint);
-                List<Tilemap> gridChildren = GetChildren(grid);
-
-                foreach (Tilemap tilemap in gridChildren)
-                {
-                    Debug.Log(tilemap);
-                    Vector3Int gridCollisionPoint = tilemap.WorldToCell(collisionPoint);
-                    TileBase wall = tilemap.GetTile(gridCollisionPoint);
-                    if (wall != null)
-                    {
-                        tilemap.SetTile(gridCollisionPoint, null);
-                    }
-
-                }
-                transform.position += (Vector3)v * 0.37f;
-            }
-        }
-        
-        
-
-    }
-    List<Tilemap> GetChildren(GameObject parent)
-    {
-        List<Tilemap> children = new List<Tilemap>();
-        foreach (Transform child in parent.transform)
-        {
-            children.Add(child.gameObject.GetComponent<Tilemap>());
-        }
-        return children;
     }
 }
--- Assets/Dungeon/Dungeon Sprites/RoomConector.cs
+++ Assets/Dungeon/Dungeon Sprites/RoomConector.cs
@@ -10,36 +10,67 @@ public class RoomConector : MonoBehaviour
     private Tilemap targetTilemap;
     public bool doorsDestroyed=false;
     public bool spawnPointMoved=false;
+    public bool colisioneperonosecumplio = false;
     void Start()
     {
         grid = GameObject.Find("Grid");
         targetTilemap=GameObject.Find("Entry Room").GetComponent<Tilemap>();
-        Invoke("DestroyThis", 5);
+        Invoke("DestroyThis", 3000);
     }
 
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-      
-        if (col.gameObject.CompareTag("SpawnPoint") && col.gameObject.GetComponent<RoomSpawner>().spawnedClosedRoom==false && !col.gameObject.GetComponent<RoomSpawner>().bossRoom && !spawnPointMoved)
+
+        if (col.gameObject.CompareTag("SpawnPoint") && col.gameObject.GetComponent<RoomSpawner>().spawnedClosedRoom == false && !col.gameObject.GetComponent<RoomSpawner>().bossRoom && !spawnPointMoved)
         {
             if (pointDirection == 1) transform.position += (Vector3)(Vector2.down * 10);
             else if (pointDirection == 2) transform.position += (Vector3)(Vector2.up * 10);
             else if (pointDirection == 3) transform.position += (Vector3)(Vector2.left * 13);
             else if (pointDirection == 4) transform.position += (Vector3)(Vector2.right * 13);
-            
+
             spawnPointMoved = true;
         }
-        else if (spawnPointMoved && !doorsDestroyed && col.name!="Closed(Closed)")
+        else if (spawnPointMoved && !doorsDestroyed && col.name != "Closed(Closed)" && col.gameObject.CompareTag("Room"))
         {
             ConnectRooms(col);
+        }
+        else
+        {
+            Vector3Int gridPoint = targetTilemap.WorldToCell(transform.position);
+            TileBase tile = targetTilemap.GetTile(gridPoint);
+            if (tile!=null && tile!=tileConector && col.name != "Closed(Closed)" && spawnPointMoved && !doorsDestroyed)
+            {
+                ConnectRooms(col);
+            }
         }
     }
     private void ConnectRooms(Collider2D col)
     {
+        StartCoroutine(esperar());
+
+
+
+    }
+    IEnumerator esperar()
+    {
         doorsDestroyed = true;
-        Vector2[] movimientosPeristalticosArray = { Vector2.right, Vector2.up, Vector2.left, Vector2.left, Vector2.down, Vector2.down, Vector2.right, Vector2.right };
-        List<Vector2> movimientosPeristalticos = new List<Vector2>(movimientosPeristalticosArray);
+        List<Vector2> movimientosPeristalticos = new List<Vector2>();
+        if (pointDirection == 1 || pointDirection == 2)
+        {
+
+            movimientosPeristalticos = new List<Vector2>(CreateMovimientosPeristalticos(11, 13));
+            movimientosPeristalticos.Insert(0, Vector2.down * 5.5f);
+            movimientosPeristalticos.Insert(1, Vector2.left * 6.5f);
+        }
+        else if (pointDirection == 3 || pointDirection == 4)
+        {
+            movimientosPeristalticos = new List<Vector2>(CreateMovimientosPeristalticos(7, 11));
+            movimientosPeristalticos.Insert(0, Vector2.down * 3.5f);
+            movimientosPeristalticos.Insert(1, Vector2.left * 5.5f);
+        }
+
+
         foreach (Vector2 mov in movimientosPeristalticos)
         {
             //Collider2D collider = Physics2D.OverlapBox(transform.position, new Vector2(0.1f, 0.1f), 0);
@@ -49,17 +80,35 @@ public class RoomConector : MonoBehaviour
             //TileBase wall = tilemap.GetTile(gridCollisionPoint);
             Vector3Int gridPoint = targetTilemap.WorldToCell(transform.position);
             TileBase tile = targetTilemap.GetTile(gridPoint);
-            if (tile != null && tile!=tileConector)
+            bool isConnected = false;
+            if (tile != null && tile != tileConector && !isConnected)
             {
                 targetTilemap.SetTile(gridPoint, null);
                 targetTilemap.SetTile(gridPoint, tileConector);
             }
-            transform.position += (Vector3)mov * 0.37f;
-            
+            else if (tile == tileConector)
+            {
+                isConnected = true;
+            }
+            transform.position += (Vector3)mov;
+            yield return new WaitForSecondsRealtime(0.05f);
+
         }
+    }
 
-
-
+    List<Vector2> CreateMovimientosPeristalticos(int repeticiones, int pasos)
+    {
+        List<Vector2> movimientos = new List<Vector2>();
+        for (int i = 0; i < repeticiones+1; i++)
+        {
+            for (int j = 0; j < pasos; j++)
+            {
+                movimientos.Add(Vector2.right);
+            }
+            movimientos.Add(Vector2.left*pasos);
+            movimientos.Add(Vector2.up);
+        }
+        return movimientos;
     }
     List<Tilemap> GetChildren(GameObject parent)
     {
