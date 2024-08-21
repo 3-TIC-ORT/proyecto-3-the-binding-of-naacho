43ff532a4a237225124b050987f1fde89b7c7e8a
Tomyatana <tomyatana@gmail.com>
2024-08-21 15:51:41 -0300

    Added destroy pickup, now have to test

--- Assets/Pickups/Scripts/HeartPickup.cs
+++ Assets/Pickups/Scripts/HeartPickup.cs
@@ -5,8 +5,10 @@ public class HeartPickup : Pickup
     public override void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Player")) {
             NaachoHeartSystem naachoLife = other.gameObject.GetComponent<NaachoHeartSystem>();
-            if(naachoLife.FindLastFullHeart() < naachoLife.GetMaxLife())
+            if(naachoLife.FindLastFullHeart() < naachoLife.GetMaxLife()){
                 naachoLife.Heal(PickupAmount);
+                Destroy(gameObject);
+            }
         }
     }
 }
