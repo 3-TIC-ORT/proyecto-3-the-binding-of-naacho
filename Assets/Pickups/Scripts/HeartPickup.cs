7feda904123e9745ac0d72c9db6ddf4e8891d8fc
Tomyatana <tomyatana@gmail.com>
2024-08-28 15:29:55 -0300

    Cleaned up if-statement

--- Assets/Pickups/Scripts/HeartPickup.cs
+++ Assets/Pickups/Scripts/HeartPickup.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 public class HeartPickup : Pickup
 {
-    bool indexInBounds(int idx, NaachoHeartSystem naachoHeartSystem) {
+    public NaachoHeartSystem naachoHeartSystem;
+    bool IndexInBounds(int idx) {
         return idx < naachoHeartSystem.GetMaxLife();
+    }
+
+    bool canBePickedUp(int lastHrtIdx) {
+        bool idxInBounds = IndexInBounds(lastHrtIdx);
+        bool hrtNotFull = naachoHeartSystem.Life[lastHrtIdx].NotIsFull();
+        return (idxInBounds && hrtNotFull) || (IndexInBounds(lastHrtIdx+1) && naachoHeartSystem.Life[lastHrtIdx+1].NotIsFull());
     }
 
     public override void OnCollisionEnter2D(Collision2D other) {
         if (!other.gameObject.CompareTag("Player")) return;
 
-        NaachoHeartSystem naachoLife = other.gameObject.GetComponent<NaachoHeartSystem>();
-        int lastHrtIdx = naachoLife.FindLastFullHeart();
+        naachoHeartSystem = other.gameObject.GetComponent<NaachoHeartSystem>();
+        int lastHrtIdx = naachoHeartSystem.FindLastFullHeart();
 
-        bool idxInBounds = indexInBounds(lastHrtIdx, naachoLife);
-        bool hrtNotFull = naachoLife.Life[lastHrtIdx].NotIsFull();
 
-        if ((idxInBounds && hrtNotFull) || (indexInBounds(lastHrtIdx+1, naachoLife) && naachoLife.Life[lastHrtIdx+1].NotIsFull()))
+        if (canBePickedUp(lastHrtIdx))
         {
-            naachoLife.Heal(PickupAmount);
+            naachoHeartSystem.Heal(PickupAmount);
             Destroy(gameObject);
         }
     }
