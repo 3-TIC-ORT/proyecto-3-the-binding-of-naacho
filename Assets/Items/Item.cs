d865eb8a5af54d2c914eef2053daf5876e773316
Tomyatana <tomyatana@gmail.com>
2024-09-06 12:11:46 -0300

    going to test items

--- Assets/Items/Item.cs
+++ Assets/Items/Item.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+abstract public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject Naacho;
+
+    public void Start() {
+        Naacho = GameObject.Find("Naacho");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void onPickup();
 }
