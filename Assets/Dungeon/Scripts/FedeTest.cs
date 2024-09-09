18dcc4321a59e129f10f394e2af856b881c5d1c1
Coolfede97 <coolfede97@gmail.com>
2024-09-09 15:37:55 -0300

    función para Tomi <3

--- Assets/Dungeon/Scripts/FedeTest.cs
+++ Assets/Dungeon/Scripts/FedeTest.cs
@@ -5,48 +5,39 @@ using UnityEngine.Tilemaps;
 public class FedeTest : MonoBehaviour
 {
     private GameObject grid;
-    bool destroyedWalls = false;
+    public Tilemap targetTilemap;
+    public TileBase tilex;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position += Vector3.right * 2;
-        Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position, new Vector2(0.5f,0.5f),0);
-        List<GameObject> walls = new List<GameObject>();
-        foreach (Collider2D collider in colliders)
+        StartCoroutine("ASD");
+    }
+    IEnumerator ASD()
+    {
+        yield return new WaitForSecondsRealtime(0);
+        changeDoorsSprite(tilex, new Vector2(0, 0));
+    }
+    // Dale el nuevo Tile para la puerta. Dale la posición del spawnPoint detectado.
+    private void changeDoorsSprite(TileBase tile, Vector2 spawnPointPos)
+    {
+        // Array con las 4 direcciones con las magnitudes correspondientes donde podría haber una puerta
+        Vector2[] fourDirections= {Vector2.up*4.5f,Vector2.down * 4.5f, Vector2.left*7.5f,Vector2.right*7.5f};
+        foreach (Vector2 direction in fourDirections)
         {
-            if (collider.gameObject.CompareTag("Room"))
+            Vector2 doorPos = spawnPointPos + direction;
+            // Door es el collider de doorTrigger
+            Collider2D door = Physics2D.OverlapBox(doorPos, new Vector2(0.5f, 0.5f),0);
+            if (door != null) 
             {
-                Debug.Log(collider.gameObject.name);
-                walls.Add(collider.gameObject);
-            }
-            else if (collider.gameObject.CompareTag("SpawnPoint"))
-            {
-                Debug.Log(collider.gameObject.name);
+                if (door.gameObject.CompareTag("DoorTrigger"))
+                {
+                    // Cambia el tile de la puerta
+                    targetTilemap.SetTile(targetTilemap.WorldToCell(doorPos), tile);
+                    // Cambia el tile de la otra puerta al restarle un poco de posición (mala prática, lo sé, pero me ganó la tarea para el cole)
+                    targetTilemap.SetTile(targetTilemap.WorldToCell(doorPos+new Vector2(-0.01f,-0.01f)), tile);
+                }
             }
         }
     }
-
-    List<GameObject> GetChildren(GameObject parent, bool filter, string tag)
-    {
-        List<GameObject> children = new List<GameObject>();
-
-        foreach (Transform child in parent.transform)
-        {
-            if (!filter)
-            {
-                children.Add(child.gameObject);
-            }
-            else if (filter && child.CompareTag(tag))
-            {
-                children.Add(child.gameObject);
-            }
-        }
-
-        return children;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Debug.Log("holdapsalsd");
-
-    }
+    
 }
