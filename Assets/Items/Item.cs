ea8f229f8439e6e5031da2a4b932611e84ef4d60
Coolfede97 <coolfede97@gmail.com>
2024-09-15 11:23:02 -0300

    animación items

--- Assets/Items/Item.cs
+++ Assets/Items/Item.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
-
+using DG.Tweening;
 abstract public class Item : MonoBehaviour
 {
     public GameObject Naacho;
-
+    private Transform transform;
+    public float animationSpeed;
+    public float heightVariation;
     public void Start() {
+        animationSpeed = 1.5f;
+        heightVariation = 0.6f;
+        transform = GetComponent<Transform>();
         Naacho = GameObject.Find("Naacho");
+        transform.DOMoveY(transform.position.y + heightVariation, animationSpeed).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
     }
-
+    private void Update()
+    {
+    }
     public abstract void onPickup();
 
 }
