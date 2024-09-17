81df6680980303d88e07eaa2503316cc97197a34
Coolfede97 <coolfede97@gmail.com>
2024-09-16 21:13:59 -0300

    oclussionCulling para los itemHolders

--- Assets/Dungeon/Scripts/ItemHolder.cs
+++ Assets/Dungeon/Scripts/ItemHolder.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class ItemHolder : MonoBehaviour
 {
     private RoomTemplates templates;
+    private GameObject naacho;
     private GameObject item;
+    private BoxCollider2D boxCollider2D;
+    public float oclussionCulling;
     void Start()
     {
+        naacho = GameObject.FindGameObjectWithTag("Player");
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         int rand = Random.Range(0, templates.items.Length);
-        item = Instantiate(templates.items[rand], transform.position, Quaternion.identity);
+        item = Instantiate(templates.items[rand], transform.position, Quaternion.identity,gameObject.transform);
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+    private void Update()
+    {
+        if ((naacho.transform.position - transform.position).magnitude < oclussionCulling) boxCollider2D.enabled = true;
+        else boxCollider2D.enabled=false;
+            
+      
     }
 
     private void OnCollisionEnter2D(Collision2D col)
