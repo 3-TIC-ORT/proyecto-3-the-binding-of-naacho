8c7860c765381a84265835d3a65a51a11f1b7dfb
Coolfede97 <coolfede97@gmail.com>
2024-09-16 21:06:15 -0300

    OclussionCulling de los items

--- Assets/Items/Item.cs
+++ Assets/Items/Item.cs
@@ -4,17 +4,25 @@ abstract public class Item : MonoBehaviour
 {
     public GameObject Naacho;
     private Transform transform;
+    private SpriteRenderer spriteRenderer;
+    [Header("Parámetros para la animación de flotar")]
     public float animationSpeed;
     public float heightVariation;
+    [Header("Otros")]
+    public float oclussionCulling;
+    
     public void Start() {
         animationSpeed = 1.5f;
         heightVariation = 0.6f;
         transform = GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         Naacho = GameObject.Find("Naacho");
         transform.DOMoveY(transform.position.y + heightVariation, animationSpeed).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
     }
     private void Update()
     {
+        if ((Naacho.transform.position - transform.position).magnitude < oclussionCulling) spriteRenderer.enabled = true;
+        else spriteRenderer.enabled = false;
     }
     public abstract void onPickup();
 
