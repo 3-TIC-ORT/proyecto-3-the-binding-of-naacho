58813800fae97fbcbf59331ac6012ccc8ccee2af
Coolfede97 <coolfede97@gmail.com>
2024-09-20 21:57:59 -0300

    avances cámara smooth

--- Assets/Dungeon/Scripts/DoorTrigger.cs
+++ Assets/Dungeon/Scripts/DoorTrigger.cs
@@ -1,3 +1,5 @@
+using Cinemachine;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +7,26 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     public bool isVertical = false;
-    public float lerpPositionDuration;
+    public float slowTimecale;
+    [Tooltip("Poner velocidad pensando que el juego estará relentizado un 90%")]
+    public float cameraTransitionSpeed;
     private BoxCollider2D collider;
     private RoomTemplates templates;
     private GameObject player;
+    private Transform cameraTransform;
+    private CinemachineBrain cm;
+    private CinemachineVirtualCamera cam;
+    private Transform cmTransform;
     public float OcclusionCullingDistance;
     private void Start()
     {
         collider = GetComponent<BoxCollider2D>();
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         player = GameObject.FindGameObjectWithTag("Player");
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        cm = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CinemachineBrain>();
+        cmTransform = GameObject.Find("Camera").GetComponent<Transform>();
+        cam = GameObject.Find("Camera").GetComponent<CinemachineVirtualCamera>();
         // Si es una puerta vertical entonces rote la puerta 90°
         if (isVertical)
         {
@@ -32,68 +44,61 @@ public class DoorTrigger : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            MoveToTheNextRoom(col.gameObject);
+            Vector3 playerPos = col.gameObject.GetComponent<Transform>().position;
+            Time.timeScale = slowTimecale;
+            cam.Follow = cameraTransform;
+            if (isVertical) MoveVertically(playerPos);
+            else MoveHorizontally(playerPos);
         }
         else if (col.gameObject.CompareTag("RoomConector")) StartCoroutine(CheckIfImNecesarry(col.gameObject));
     }
     
-    // Mueve al jugador a la siguiente habitación. ¿Viste? que locura.
-    private void MoveToTheNextRoom(GameObject player)
+    
+    private void MoveVertically(Vector3 playerPos)
     {
-        Vector3 playerPos = player.GetComponent<Transform>().position;
-        if (isVertical) 
+        float nextDoorDistance = templates.verticalDoorToDoorRoomArea.y + 0.1f;
+        // Identificar si el jugador está a la abajo de este objeto (puerta) o no
+        // y así mover el jugador para arriba o a la abajo (en este caso que es una puerta vertical).
+        if (playerPos.y - transform.position.y < 0)
         {
-            float nextDoorDistance = templates.verticalDoorToDoorRoomArea.y+0.1f;
-            // Identificar si el jugador está a la abajo de este objeto (puerta) o no
-            // y así mover el jugador para arriba o a la abajo (en este caso que es una puerta vertical).
-            if (playerPos.y - transform.position.y<0)
-            {
-                playerPos.y += nextDoorDistance;
-                player.GetComponent<Transform>().position = playerPos;
-            }
-            else
-            {
-                playerPos.y -= nextDoorDistance;
-                player.GetComponent<Transform>().position = playerPos;
-            }
+            cameraTransform.DOMoveY(cameraTransform.position.y + nextDoorDistance, cameraTransitionSpeed).SetEase(Ease.Linear).onComplete = ()=>ManageCameraAndTime(true, 1, playerPos);
+            playerPos.y += nextDoorDistance;
+            player.GetComponent<Transform>().position = playerPos;
         }
         else
         {
-            float nextDoorDistance = templates.horizontalDoorToDoorRoomArea.x+0.1f;
-            if (playerPos.x - transform.position.x < 0)
-            {
-                playerPos.x += nextDoorDistance;
-                player.GetComponent<Transform>().position = playerPos;
-
-            }
-            else
-            {
-                playerPos.x -= nextDoorDistance;
-                player.GetComponent<Transform>().position = playerPos;
-            }
+            cameraTransform.DOMoveY(cameraTransform.position.y - nextDoorDistance, cameraTransitionSpeed).SetEase(Ease.Linear).onComplete = () => ManageCameraAndTime(true, 1, playerPos);
+            playerPos.y -= nextDoorDistance;
+            player.GetComponent<Transform>().position = playerPos;
         }
     }
-    // Mueve al jugador de una habitación a otra.
-    // initialAxis: valor del eje (X si es horizontal, Y si es vertical) del jugador
-    // finalAxis: valor del eje del otro lado de la puerta
-    // duration: que tan rápido será la transición (segundos)
-    // player: no lo se
-    // isHorizontal: para saber si el eje es el eje X o Y.
-    IEnumerator LerpPosition(float initialAxis, float finalAxis, float duration, GameObject player, bool isHorizontal)
+    private void MoveHorizontally(Vector3 playerPos)
     {
-        float time=0;
-        float currentAxis;
-        Vector3 playerPos = player.GetComponent<Transform>().position;
-        while (time<duration)
-        {
-            currentAxis = Mathf.Lerp(initialAxis, finalAxis, time / duration);
-            if (isHorizontal) playerPos.x = currentAxis;
-            else playerPos.y = currentAxis;
-            player.GetComponent<Transform>().position = playerPos;
+        float nextDoorDistance = templates.horizontalDoorToDoorRoomArea.x + 0.1f;
 
-            time += Time.deltaTime;
-            yield return null;
+        if (playerPos.x - transform.position.x < 0)
+        {
+            cameraTransform.DOMoveX(cameraTransform.position.x + nextDoorDistance, cameraTransitionSpeed).SetEase(Ease.Linear).onComplete = () => ManageCameraAndTime(true, 1, playerPos);
+            playerPos.x += nextDoorDistance;
+            player.GetComponent<Transform>().position = playerPos;
         }
+        else
+        {
+            cameraTransform.DOMoveX(cameraTransform.position.x - nextDoorDistance, cameraTransitionSpeed).SetEase(Ease.Linear).onComplete = () => ManageCameraAndTime(true, 1,playerPos);
+            playerPos.x -= nextDoorDistance;
+            player.GetComponent<Transform>().position = playerPos;
+        }
+    }
+    // Mueve al jugador a la siguiente habitación. ¿Viste? que locura.
+    private void ManageCameraAndTime(bool cmEnabled, float worldSpeed, Vector3 playerPos)
+    {
+        Time.timeScale = worldSpeed;
+        StartCoroutine(EnableCinemachineAfterAFrame(cmEnabled));
+    }
+    IEnumerator EnableCinemachineAfterAFrame(bool cmEnabled)
+    {
+        yield return null; 
+        cam.Follow = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     // Se llama cuando se colisiona con un roomConector. Si las dos habitaciones están conectadas entonces soy necesario y no me destruyo
     // En el caso contrario, me destruyo porque como no están conectadas y por consiguiente se van a conectar formando una habitación
