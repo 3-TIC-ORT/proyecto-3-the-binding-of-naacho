8bc9c95d1d4a8b4e6426d85ca340ef6434d0c1dc
Coolfede97 <coolfede97@gmail.com>
2024-09-25 10:33:53 -0300

    enemigo naachotortuga terminado

--- Assets/Dungeon/Enemies/Scripts/NaachoTortuga.cs
+++ Assets/Dungeon/Enemies/Scripts/NaachoTortuga.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class NaachoTortuga : Enemy
 {
+    private Animator animator;
+    private SpriteRenderer sr;
     public override void Update()
     {
         if (GameManager.Instance.stop) return;
         Vector2 playerDir = (Vector2)(Player.transform.position - transform.position).normalized;
         rb2D.velocity = Speed * Time.deltaTime * playerDir;
         CheckMainDirection(playerDir);
+    }
+    public override void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        Player = GameObject.Find("Naacho");
     }
 
     private void CheckMainDirection(Vector3 direction)
@@ -19,10 +28,16 @@ public class NaachoTortuga : Enemy
             if (direction.y>0)
             {
                 Debug.Log("Me muevo hacia arriba");
+                animator.SetBool("up", true);
+                animator.SetBool("down",false);
+                animator.SetBool("horizontal", false);
             }
             else
             {
                 Debug.Log("Me muevo hacia abajo");
+                animator.SetBool("down", true);
+                animator.SetBool("up", false);
+                animator.SetBool("horizontal", false);
             }
         }
         else
@@ -30,10 +45,19 @@ public class NaachoTortuga : Enemy
             if (direction.x>0)
             {
                 Debug.Log("Me muevo hacia la derecha");
+                sr.flipX = true;
+                animator.SetBool("horizontal", true);
+                animator.SetBool("up", false);
+                animator.SetBool("down", false);
+
             }
             else
             {
                 Debug.Log("Me muevo hacia la izquierda");
+                sr.flipX = false;
+                animator.SetBool("horizontal", true);
+                animator.SetBool("up", false);
+                animator.SetBool("down", false);
             }
         }
     }
