56836cc8e1902ea47c349784b15724d804009382
Coolfede97 <coolfede97@gmail.com>
2024-10-01 09:12:18 -0300

    Commit innesesario

--- Assets/Dungeon/Scripts/FedeTest.cs
+++ Assets/Dungeon/Scripts/FedeTest.cs
@@ -5,11 +5,13 @@ using UnityEngine.Rendering.Universal;
 using UnityEngine.Tilemaps;
 public class FedeTest : MonoBehaviour
 {
+    public Vector3 paraDondeMeMuevo; 
     private void Start() {
         Debug.Log("PUPUPUI");
+        transform.Translate(paraDondeMeMuevo);
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("COLISIONNNN");
+        Debug.Log("COLISIONNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE   ");
     }
 }
