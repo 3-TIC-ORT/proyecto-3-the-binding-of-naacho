4d2e1092ab37ae502b8ea77dea6f67572ebf2ec4
Tomás Atanasoff <tomyatana@gmail.com>
2024-10-03 13:19:27 -0300

    Lechuguita terminada

---
+++ Assets/Dungeon/Enemies/Scripts/Lechuguita.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lechuguita : Enemy
+{
+    private Animator animator;
+    public override void Start()
+    {
+        base.Start();
+        animator = gameObject.GetComponent<Animator>();
+    }
+    public override void Update()
+    {
+        base.Update();
+        if (GameManager.Instance.stop) return;  
+        Vector2 playerDir = (Vector2)(Player.transform.position - transform.position).normalized;
+        rb2D.velocity = Speed * Time.deltaTime * playerDir;
+        animator.SetBool("isMoving", true);
+        CheckMainDirection(playerDir);
+    }
+
+    private void CheckMainDirection(Vector3 direction)
+    {
+        if (direction.x > 0)
+        {
+            SpRenderer.flipX = false;
+        }
+        else
+        {
+            SpRenderer.flipX = true;
+        }
+    }
+}
