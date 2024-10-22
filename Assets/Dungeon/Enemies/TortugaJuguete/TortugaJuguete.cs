d642dd929b274b53a2efba33f31f17376ce78e7e
Federico Ugarte <coolfede97@gmail.com>
2024-10-22 15:50:52 -0300

    ajuste de parámetros de la tortuga juguete. No recibe knockback al moverse

--- Assets/Dungeon/Enemies/TortugaJuguete/TortugaJuguete.cs
+++ Assets/Dungeon/Enemies/TortugaJuguete/TortugaJuguete.cs
@@ -47,8 +47,10 @@ public class TortugaJuguete : Enemy
                 Vector2 randomDirection = GetRandomDirection();
                 rb2D.velocity = randomDirection*Speed*Time.deltaTime;
             }
+            hasKnockback = false;
             yield return new WaitForSeconds(walkDuration);
             rb2D.velocity=Vector2.zero;
+            hasKnockback = true;
         }
     }
     private Vector2 GetRandomDirection()
