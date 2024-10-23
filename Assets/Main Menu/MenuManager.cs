c2720c5cf00375a39ebdc54c37843a70d9ec35d4
Federico Ugarte <coolfede97@gmail.com>
2024-10-23 10:07:19 -0300

    menus con transiciones

--- Assets/Main Menu/MenuManager.cs
+++ Assets/Main Menu/MenuManager.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 public class MenuManager : MonoBehaviour
 {
+    public float transitionSpeed;
+    private Image transitionScreen;
+
+    private void Start()
+    {
+        transitionScreen = GameObject.FindGameObjectWithTag("TransitionScreen").GetComponent<Image>();
+        transitionScreen.DOFade(0, transitionSpeed / 2).onComplete=()=> { transitionScreen.gameObject.SetActive(false);};
+    }
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        transitionScreen.gameObject.SetActive(true);
+        transitionScreen.DOFade(1, transitionSpeed / 2).onComplete = () =>
+        {
+            SceneManager.LoadScene(sceneName);
+        };
     }
     public void ExitGame() { Application.Quit(); }
 }
