ee55a2986764742a4ce1b818b022f049956b228d
Tomyatana <tomyatana@gmail.com>
2024-11-15 10:12:40 -0300

    Nachorazones

--- Assets/UI/Scripts/TextTest.cs
+++ Assets/UI/Scripts/TextTest.cs
@@ -16,16 +16,24 @@ public class TextTest : MonoBehaviour
     // Update is called once per frame
     public void UIUpdate(float life)
     {
-        GameObject heartimg= null;
+        GameObject heartimg = null;
+        foreach(Transform child in transform) {
+            Destroy(child.gameObject);
+        }
         foreach(Heart heart in naachoHeartSystem.Life) {
             if(heart == null) continue;
+
             GameObject hrt = new GameObject("Heart");
+
             if(heartimg == null)
                 hrt.transform.position = transform.position;
             else hrt.transform.position = heartimg.transform.position;
+
             hrt.transform.localScale = Vector3.one * 0.5f;
-            hrt.AddComponent<Image>().sprite = img.sprite;
-            hrt.transform.position += Vector3.right * 3;
+            Image image = hrt.AddComponent<Image>();
+            image.sprite = img.sprite;
+            image.color = new Color(img.color.r, img.color.g, img.color.b, heart.Amount);
+            hrt.transform.position += Vector3.right * 50;
             hrt.transform.SetParent(transform);
             heartimg = hrt;
         }
